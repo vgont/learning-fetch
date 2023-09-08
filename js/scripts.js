978b08b0c05d1117c564da1bf2f454149d5c39f4
vgont <vgontijo34@gmail.com>
2023-09-08 15:39:07 -0300

    ending project

---
+++ js/scripts.js
@@ -0,0 +1,130 @@
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+//Setting elements
+const loadingElement = document.querySelector('#loading');
+const postsContainer = document.querySelector('#posts-container');
+
+const postPage = document.querySelector('#post');
+const postContainter = document.querySelector('#post-container');
+const commentsContainter = document.querySelector('#comments-container');
+
+const commentForm = document.querySelector('#coment-form') 
+const emailInput = document.querySelector('#email')
+const bodyInput = document.querySelector('#body')
+//Get id from URL
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+//Get all posts
+async function getAllPosts() {
+    const response = await fetch(url);
+
+    console.log(response);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    loadingElement.classList.add("hide")
+
+    data.map((post) => {
+
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Ler";
+        link.setAttribute("href", `/post.html?id=${post.id}`);
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        postsContainer.appendChild(div);
+    })
+}
+
+//Get individual post
+async function getPost(id) {
+
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`),
+    ])
+
+    const dataPost = await responsePost.json();
+    console.log(dataPost)
+
+    const dataComments = await responseComments.json();
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+    const div = document.createElement("div");
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainter.appendChild(title);
+    postContainter.appendChild(body);
+
+    console.log(dataComments);
+
+    dataComments.map((comment) => {
+        createComment(comment);        
+    })
+}
+
+function createComment(comment){
+    const div = document.createElement("div")
+    const email = document.createElement("h3");
+    const commentBody = document.createElement("p");    
+
+    email.innerText = comment.email;
+    commentBody.innerText = comment.body;
+
+    div.appendChild(email);
+    div.appendChild(commentBody);
+
+    commentsContainter.appendChild(div);
+}
+
+// Post a comment
+async function postComment(comment){
+
+    const response = await fetch(`${url}/${postId}/comments`, {
+        method: "POST",
+        body: comment,
+        headers: {
+            "Content-type": "application/json", 
+        },
+    });
+
+    const data = await response.json();
+    createComment(data);
+}
+
+if (!postId) {
+    getAllPosts();
+} else {
+    getPost(postId);
+
+    // Add event to comment form
+    commentForm.addEventListener("submit", (e)=>{
+        e.preventDefault();
+
+        let comment = {
+            email: emailInput.value,
+            body: bodyInput.value            
+        }
+        comment = JSON.stringify(comment)
+
+        postComment(comment);
+    })
+
+}
